5e4689fd0085e120c3c7815f98a926bb2d1bf2c6
tarasgavr <tarasgavrilov1985@gmail.com>
2024-02-27 17:01:50 +0200

    tasks solved

--- js-train-24/10.js
+++ js-train-24/10.js
@@ -2,13 +2,13 @@ console.log("Завдання: 10 ==============================");
 
 function task10() {
   const promise1 = new Promise((resolve, reject) => {
-    setInterval(resolve, 500, "Promise 1");
+    setTimeout(resolve, 500, "Promise 1");
   });
   const promise2 = new Promise((resolve, reject) => {
-    setInterval(resolve, 200, "Promise 2");
+    setTimeout(resolve, 200, "Promise 2");
   });
   const promise3 = new Promise((resolve, reject) => {
-    setInterval(resolve, 300, "Promise 3");
+    setTimeout(resolve, 300, "Promise 3");
   });
   Promise.any([promise1, promise2, promise3])
     .then((result) => {
