6a12cc0052d5a1d8e1fc3ca2e615551fa9ad62ed
tarasgavr <tarasgavrilov1985@gmail.com>
2024-03-17 18:21:45 +0200

    tasks solved

--- js-crud/1-user/src/route/index.js
+++ js-crud/1-user/src/route/index.js
@@ -18,10 +18,16 @@ class User {
     return this.#userList
   }
   static deleteUser = (user) => {
-    this.#userList.push(user)
+    const index = this.#userList.findIndex(
+      (value) => value === user,
+    )
+    this.#userList.splice(index, 1)
   }
   static updateUser = (user) => {
-    this.#userList.push(user)
+    const index = this.#userList.findIndex(
+      (value) => value === user,
+    )
+    this.#userList.splice(index, 1)
   }
 }
 // ================================================================
@@ -46,7 +52,6 @@ router.get('/', function (req, res) {
   })
   // ↑↑ сюди вводимо JSON дані
 })
-
 // ================================================================
 router.post('/user-create', function (req, res) {
   const { login, email, password } = req.body
@@ -54,6 +59,24 @@ router.post('/user-create', function (req, res) {
   User.addUser(user)
   res.render('user-create', {
     style: 'user-create',
+  })
+})
+// ================================================================
+router.post('/user-delete', function (req, res) {
+  const { login, email, password } = req.body
+  const user = new User(login, email, password)
+  User.deleteUser(user)
+  res.render('user-delete', {
+    style: 'user-delete',
+  })
+})
+// ================================================================
+router.post('/user-update', function (req, res) {
+  const { login, email, password } = req.body
+  const user = new User(login, email, password)
+  User.updateUser(user)
+  res.render('user-update', {
+    style: 'user-update',
   })
 })
 
