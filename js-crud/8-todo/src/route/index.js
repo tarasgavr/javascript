656b9cf161c3477cbc2b32ef59336efaa03c6c9d
taureau <tarasgavrilov1985@gmail.com>
2024-08-04 19:55:36 +0300

    file changed

--- js-crud/8-todo/src/route/index.js
+++ js-crud/8-todo/src/route/index.js
@@ -3,11 +3,11 @@ const express = require('express')
 const router = express.Router()
 
 // Підключіть файли роутів
-const  site = require('./site')
+const  todolist = require('./todolist')
 // Підключіть інші файли роутів, якщо є
 
 // Об'єднайте файли роутів за потреби
-router.use('/site',  site)
+router.use('/todolist',  todolist)
 // Використовуйте інші файли роутів, якщо є
 
 // Експортуємо глобальний роутер
